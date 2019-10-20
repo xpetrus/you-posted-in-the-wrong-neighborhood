c08a80a0bf5ff9f36429de39c563ad1e4cda8e4b
xpetrus <phsiao24@gmail.com>
2019-10-19 21:15:20 -0400

    frontend initial

--- humbleME/src/app/app.module.ts
+++ humbleME/src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChatboxesComponent } from './chatboxes/chatboxes.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'chat', component: ChatboxesComponent
+  },
+  {
+    path: '',
+    redirectTo: '/chat',
+    pathMatch: 'full'
+  },
+  {
+    path: '**', component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatboxesComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }  // this is a debugging thing
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ humbleME/src/app/chatboxes/chatboxes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatboxes',
+  templateUrl: './chatboxes.component.html',
+  styleUrls: ['./chatboxes.component.css']
+})
+export class ChatboxesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
